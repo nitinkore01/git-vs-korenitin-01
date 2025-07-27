24fcbf31907688e14648241693d73815392ede34
omik6755 <omik6755@gmail.com>
2025-07-27 23:19:54 +0530

    c5

--- src/test1.py
+++ src/test1.py
@@ -1,8 +1,9 @@
 print("test11")
 print("test")
 
-
 def test_function():
     print("hello")
     if True:
         print("test22")
+
+print("test33")
--- src/test2.py
+++ src/test2.py
@@ -1,6 +1,7 @@
 print("test22")
 if True:
+    print("test1")
     print("test2")
+    print("test3")
 print("test")
 print("test222")
-    
